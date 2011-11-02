3fb2f19a1628674c47f538ca422efd545824cae8
Kevin Dees <kevin@robojuice.com>
2011-11-02 11:32:39 -0400

    fixed and cleanup defaults and make method

--- post_type.php
+++ post_type.php
@@ -4,24 +4,12 @@ class post_type extends acpt {
 	/**
 	 * Make Post Type. Do not use before init.
 	 * 
-	 * $settings = array(
-	 *	'public' => true,
-	 *	'has_archive' => true,
-	 *	'show_ui' => true,
-	 *	'taxonomies' => array( ... ),
-	 *	'supports' => array( ... ),
-	 *	'hierarchical' => true,
-	 * );
-	 * 
-	 * @param string $post_type post type name
-	 * @param array $settings settings
-	 * @param string $singular singular name
-	 * @param string $plural plural name
+	 * @param string $singular singular name is required
+	 * @param string $plural plural name is required
+	 * @param buleon $cap turn on custom capabilities
+	 * @param array $settings args override and extend
 	 */
-	function make($post_type, $settings, $singular, $plural) {
-		// Test for param
-		if(!$post_type) exit('Making Post Type: You need to enter a post type.');
-		if(!$settings) exit('Making Post Type: You need to enter a settings.');
+	function make($singular, $plural, $cap, $settings) {
 		if(!$singular) exit('Making Post Type: You need to enter a singular name.');
 		if(!$plural) exit('Making Post Type: You need to enter a plural name.');
 		
@@ -58,20 +46,25 @@ class post_type extends acpt {
 		$args = array(
 			'labels' => $labels,
 			'description' => $plural,
-			// 'capability_type' => $singular,
-			// 'capabilities' => $capabilities,
 			'rewrite' => array( 'slug' => $plural),
-			
-			'public' => $settings['public'], // Boolean
-			'has_archive' => $settings['has_archive'], // Boolean
-			'hierarchical' => $settings['hierarchical'], // Boolean
-			'show_ui' => $settings['show_ui'], // Boolean
-			'taxonomies' => $settings['taxonomies'], // Array
-			'supports' => $settings['supports'], // Array
+			'public' => true,
+			'has_archive' => true,
 		);
 		
+		if(!$settings) $settings = array();
+		if($cap === true) :
+			$cap = array(
+				'capability_type' => $singular,
+				'capabilities' => $capabilities,
+			);
+		else :
+			$cap = array();
+		endif;
+		
+		$args = array_merge($args, $cap, $settings);
+		
 		// Register post type
-		register_post_type( $post_type, $args);
+		register_post_type($singular, $args);
 	}
 	
 	/**
