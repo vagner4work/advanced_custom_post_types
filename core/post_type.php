c9a71e62f7c6068c4409f4d917bb7a448b9658a5
Kevin Dees <kevindees@1015800.no-reply.drupal.org>
2013-04-03 01:31:21 -0400

    add icon function

--- core/post_type.php
+++ core/post_type.php
@@ -22,14 +22,76 @@ class post_type extends acpt {
 
 	public $singular = null;
 	public $plural = null;
+	public $icon = null;
+	public $icon_pos = array(
+		'notebook' => array('a' => '-7px -8px'),
+		'refresh' => array('a' => '-37px -9px'),
+		'thumbs-up' => array('a' => '-67px -8px'),
+		'box' => array('a' => '-93px -9px'),
+		'bug' => array('a' => '-144px -10px'),
+		'cake' => array('a' => '-167px -11px'),
+		'calendar' => array('a' => '-188px -8px'),
+		'card-biz' => array('a' => '-237px -10px'),
+		'task' => array('a' => '-262px -8px'),
+		'clock' => array('a' => '-286px -8px'),
+		'color' => array('a' => '-310px -9px'),
+		'compass' => array('a' => '-333px -9px'),
+		'dine' => array('a' => '-357px -8px'),
+		'ipad' => array('a' => '-378px -8px'),
+		'ticket' => array('a' => '-399px -8px'),
+		'shirt' => array('a' => '-427px -9px'),
+		'pulse' => array('a' => '-449px -9px'),
+		'card-play' => array('a' => '-471px -8px'),
+		'dine-plate' => array('a' => '-492px -8px'),
+		'pill' => array('a' => '-517px -10px'),
+		'plane' => array('a' => '-538px -11px'),
+		'paint' => array('a' => '-564px -9px'),
+		'mic' => array('a' => '-588px -8px'),
+		'location' => array('a' => '-612px -8px'),
+		'leaf' => array('a' => '-629px -10px'),
+		'music' => array('a' => '-650px -9px'),
+		'wine' => array('a' => '-672px -8px'),
+		'dashboard' => array('a' => '-695px -11px'),
+		'person' => array('a' => '-719px -9px'),
+		'weather' => array('a' => '-742px -10px')
+	);
 
-	function __construct( $singular = null, $plural = null, $cap = false, $settings = array() ) {
+	function __construct( $singular = null, $plural = null, $cap = false, $settings = array(), $icon = null ) {
 		if($singular !== null ) $this->make($singular, $plural, $cap, $settings);
 	}
-	
+
+	function icon($name) {
+		if(!$singular) exit('Adding Icon: You need to enter an icon name.');
+
+		$this->icon = $name;
+
+		add_action( 'wp_head', 'set_icon_css');
+
+	}
+
+	function set_icon_css() { ?>
+
+		<style type="text/css">
+			#adminmenu #menu-posts-<?php echo $this->singular; ?> .wp-menu-image {
+			  background-image: url(../img/menu.png);
+			}
+
+			#adminmenu #menu-posts-<?php echo $this->singular; ?> .wp-menu-image {
+			  background-position: -29px -33px;
+			}
+
+			#adminmenu #menu-posts-<?php echo $this->singular; ?>:hover div.wp-menu-image,
+			#adminmenu #menu-posts-<?php echo $this->singular; ?>.wp-has-current-submenu div.wp-menu-image,
+			#adminmenu #menu-posts-<?php echo $this->singular; ?>.current div.wp-menu-image {
+			  background-position: <?php echo $icon_pos[$this->icon][a]; ?>;
+			}
+		</style>
+
+	<?php }
+
 	/**
 	 * Make Post Type. Do not use before init.
-	 * 
+	 *
 	 * @param string $singular singular name is required
 	 * @param string $plural plural name is required
 	 * @param boolean $cap turn on custom capabilities
@@ -46,7 +108,7 @@ class post_type extends acpt {
 		// setup object for later use
 		$this->plural = $plural;
 		$this->singular = $singular;
-		
+
 		// make uppercase
 		$upperSingular = ucwords($singular);
 		$upperPlural = ucwords($plural);
@@ -61,11 +123,11 @@ class post_type extends acpt {
 			'view_item' => 'View '.$upperSingular,
 			'search_items' => 'Search '.$upperPlural,
 			'not_found' =>  'No '.$plural.' found',
-			'not_found_in_trash' => 'No '.$plural.' found in Trash', 
+			'not_found_in_trash' => 'No '.$plural.' found in Trash',
 			'parent_item_colon' => '',
 			'menu_name' => $upperPlural,
 		);
-		
+
 		$capabilities = array(
 			'publish_posts' => 'publish_'.$plural,
 			'edit_post' => 'edit_'.$singular,
@@ -77,7 +139,7 @@ class post_type extends acpt {
 			'read_post' => 'read_'.$singular,
 			'read_private_posts' => 'read_private_'.$plural,
 		);
-		
+
 		if($cap === true) :
 			$cap = array(
 				'capability_type' => $singular,
@@ -86,7 +148,7 @@ class post_type extends acpt {
 		else :
 			$cap = array();
 		endif;
-		
+
 		$args = array(
 			'labels' => $labels,
 			'description' => $plural,
@@ -94,9 +156,9 @@ class post_type extends acpt {
 			'public' => true,
 			'has_archive' => true,
 		);
-		
+
 		$args = array_merge($args, $cap, $settings);
-		
+
 		// Register post type
 		register_post_type($singular, $args);
 	}
