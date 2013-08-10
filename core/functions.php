1e9391236137eb35a1253edc0e9a99bc35feca14
Kevin Dees <kevindees@Kevins-MacBook-Pro.local>
2013-08-10 10:51:59 -0400

    inline doc, fix echo function bug

--- core/functions.php
+++ core/functions.php
@@ -2,6 +2,10 @@
 /**
  * Get Meta Access Function
  *
+ * Get the post meta out of the DB table {prefix}_postmeta.
+ * Though it is only a interface for acpt_get::meta it is the preferred method to
+ * access data within theme template and plugin files.
+ *
  * @param string $name
  * @param string $fallBack
  * @param bool $groups
@@ -13,8 +17,18 @@ function acpt_meta($name = '', $fallBack = '', $groups = true, $id = null) {
   return acpt_get::meta($name, $fallBack, $groups, $id);
 }
 
-function e_acpt_meta($name = '', $fallBack = '', $groups = true, $theID = null) {
-  $data = acpt_meta($name, $groups, $fallBack, $theID);
+/**
+ * Echo Meta Data
+ *
+ * Echo a single string value gotten from acpt_meta().
+ *
+ * @param string $name
+ * @param string $fallBack
+ * @param bool $groups
+ * @param null $id
+ */
+function e_acpt_meta($name = '', $fallBack = '', $groups = true, $id = null) {
+  $data = acpt_meta($name, $fallBack, $groups, $id);
   ($fallBack !== '' ) ? true : $fallBack = 'No string data '.$name;
   is_string($data) ? true : $data = $fallBack;
   echo $data;
